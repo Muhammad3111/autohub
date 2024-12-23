b324eff714d86cf34f784561ae9ae01718a18a09
Muhammad3111 <karimovmuhammad3502@gmail.com>
2024-12-23 09:32:20 +0500

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import SignIn from "./pages/sign-in/SignIn";
 import SignUp from "./pages/sign-up/SignUp";
