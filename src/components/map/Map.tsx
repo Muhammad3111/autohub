352f70e9f6dbcccca7a9a7e40da59380718f8c8d
Muhammadjon <pofessional6233@gmail.com>
2025-01-18 18:23:27 +0500

    add dealer page

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,84 +1,57 @@
-import React, { useEffect, useState } from "react";
-import Map, { Marker } from "react-map-gl";
+import React, { useState } from "react";
+import Map, {
+    Marker,
+    NavigationControl,
+    FullscreenControl,
+} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const MAPBOX_TOKEN = import.meta.env.VITE_MAP_TOKEN;
 
 const MapComponent: React.FC = () => {
-    const [viewport, setViewport] = useState({
-        latitude: 41.005512,
-        longitude: 71.661624,
+    const fixedLatitude = 41.005512;
+    const fixedLongitude = 71.661624;
+
+    const [viewport, _setViewport] = useState({
+        latitude: fixedLatitude,
+        longitude: fixedLongitude,
         zoom: 12,
     });
-    const [userLocation, setUserLocation] = useState<{
-        latitude: number;
-        longitude: number;
-    } | null>(null);
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    setUserLocation({
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude,
-                    });
-                    setViewport({
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude,
-                        zoom: 13,
-                    });
-                },
-                (error) => {
-                    setErrorMessage(
-                        "Unable to retrieve your location. Please check your permissions."
-                    );
-                    console.error("Geolocation error:", error.message);
-                }
-            );
-        } else {
-            setErrorMessage("Geolocation is not supported by your browser.");
-        }
-    }, []);
 
     return (
         <div className="relative w-full h-[550px] overflow-hidden shadow-lg rounded-md">
-            {errorMessage && (
-                <div className="absolute top-4 left-4 bg-red-500 text-white text-sm p-3 rounded-md shadow-md z-10">
-                    {errorMessage}
-                </div>
-            )}
             <Map
                 initialViewState={viewport}
                 style={{ width: "100%", height: "100%" }}
                 mapStyle="mapbox://styles/mapbox/streets-v11"
                 mapboxAccessToken={MAPBOX_TOKEN}
             >
-                {userLocation && (
-                    <Marker
-                        latitude={userLocation.latitude}
-                        longitude={userLocation.longitude}
-                        anchor="bottom"
-                    >
-                        <div className="animate-bounce">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="red"
-                                viewBox="0 0 24 24"
-                                strokeWidth="2"
-                                stroke="currentColor"
-                                className="w-8 h-8"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M12 2c4.42 0 8 3.58 8 8 0 6.1-8 12-8 12s-8-5.9-8-12c0-4.42 3.58-8 8-8zm0 10c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"
-                                />
-                            </svg>
-                        </div>
-                    </Marker>
-                )}
+                <Marker
+                    latitude={fixedLatitude}
+                    longitude={fixedLongitude}
+                    anchor="bottom"
+                >
+                    <div className="animate-bounce">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="red"
+                            viewBox="0 0 24 24"
+                            strokeWidth="2"
+                            stroke="currentColor"
+                            className="w-8 h-8"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 2c4.42 0 8 3.58 8 8 0 6.1-8 12-8 12s-8-5.9-8-12c0-4.42 3.58-8 8-8zm0 10c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"
+                            />
+                        </svg>
+                    </div>
+                </Marker>
+
+                <NavigationControl position="bottom-right" />
+
+                <FullscreenControl position="top-right" />
             </Map>
         </div>
     );
--- src/pages/dealers/Dealers.tsx
+++ src/pages/dealers/Dealers.tsx
@@ -4,9 +4,116 @@ import Header from "../../components/header/Header";
 const Dealers = () => {
     return (
         <div>
-            <Header title="Dealer" />
-            <div className="my-container py-20">
-                <h1>Dealer</h1>
+            <Header title="Dealers" />
+            <div className="my-container py-20 flex flex-col gap-5">
+                <div className="w-full bg-white p-5 hover:bg-primary hover:bg-opacity-5 border hover:border-primary duration-150 flex justify-between">
+                    <div className="flex gap-8">
+                        <img
+                            src="https://chinaglobalsouth.com/dev/wp-content/uploads/2023/11/BYD-Thailand-768x464.jpg"
+                            alt=""
+                            className="cursor-pointer h-[150px] object-cover"
+                            width={200}
+                        />
+
+                        <div className="flex flex-col gap-4">
+                            <h1 className="font-normal text-primary text-lg cursor-pointer">
+                                BYD Dynasty Ruipeng Hongda Pingsan Store
+                            </h1>
+                            <p className="text-[#666666] text-sm">
+                                Main brands: {"BYD"}{" "}
+                                {"A total of 97 models on sale"}
+                            </p>
+                            <p className="text-[#666666] text-sm">
+                                electricity talk: {"77 269 47 77"}
+                            </p>
+                            <p className="text-[#666666] text-sm">
+                                landsite: 1st-2nd Floor, Building 3, Xingye
+                                Road, Donggaocun
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <button className="border border-primary bg-[#F6F9FC] text-primary w-[105px] p-2 rounded hover:bg-primary hover:bg-opacity-55 hover:text-white duration-150 text-sm text-left pl-4">
+                            Enter the store
+                        </button>
+                        <button className="border bg-primary w-[105px] p-2 rounded hover:bg-primary-hover duration-150 text-sm text-white text-left pl-4">
+                            Get the reserve price
+                        </button>
+                    </div>
+                </div>
+                <div className="w-full bg-white p-5 hover:bg-primary hover:bg-opacity-5 border hover:border-primary duration-150 flex justify-between">
+                    <div className="flex gap-8">
+                        <img
+                            src="https://chinaglobalsouth.com/dev/wp-content/uploads/2023/11/BYD-Thailand-768x464.jpg"
+                            alt=""
+                            className="cursor-pointer h-[150px] object-cover"
+                            width={200}
+                        />
+
+                        <div className="flex flex-col gap-4">
+                            <h1 className="font-normal text-primary text-lg cursor-pointer">
+                                BYD Dynasty Ruipeng Hongda Pingsan Store
+                            </h1>
+                            <p className="text-[#666666] text-sm">
+                                Main brands: {"BYD"}{" "}
+                                {"A total of 97 models on sale"}
+                            </p>
+                            <p className="text-[#666666] text-sm">
+                                electricity talk: {"77 269 47 77"}
+                            </p>
+                            <p className="text-[#666666] text-sm">
+                                landsite: 1st-2nd Floor, Building 3, Xingye
+                                Road, Donggaocun
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <button className="border border-primary bg-[#F6F9FC] text-primary w-[105px] p-2 rounded hover:bg-primary hover:bg-opacity-55 hover:text-white duration-150 text-sm text-left pl-4">
+                            Enter the store
+                        </button>
+                        <button className="border bg-primary w-[105px] p-2 rounded hover:bg-primary-hover duration-150 text-sm text-white text-left pl-4">
+                            Get the reserve price
+                        </button>
+                    </div>
+                </div>
+                <div className="w-full bg-white p-5 hover:bg-primary hover:bg-opacity-5 border hover:border-primary duration-150 flex justify-between">
+                    <div className="flex gap-8">
+                        <img
+                            src="https://chinaglobalsouth.com/dev/wp-content/uploads/2023/11/BYD-Thailand-768x464.jpg"
+                            alt=""
+                            className="cursor-pointer h-[150px] object-cover"
+                            width={200}
+                        />
+
+                        <div className="flex flex-col gap-4">
+                            <h1 className="font-normal text-primary text-lg cursor-pointer">
+                                BYD Dynasty Ruipeng Hongda Pingsan Store
+                            </h1>
+                            <p className="text-[#666666] text-sm">
+                                Main brands: {"BYD"}{" "}
+                                {"A total of 97 models on sale"}
+                            </p>
+                            <p className="text-[#666666] text-sm">
+                                electricity talk: {"77 269 47 77"}
+                            </p>
+                            <p className="text-[#666666] text-sm">
+                                landsite: 1st-2nd Floor, Building 3, Xingye
+                                Road, Donggaocun
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <button className="border border-primary bg-[#F6F9FC] text-primary w-[105px] p-2 rounded hover:bg-primary hover:bg-opacity-55 hover:text-white duration-150 text-sm text-left pl-4">
+                            Enter the store
+                        </button>
+                        <button className="border bg-primary w-[105px] p-2 rounded hover:bg-primary-hover duration-150 text-sm text-white text-left pl-4">
+                            Get the reserve price
+                        </button>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
