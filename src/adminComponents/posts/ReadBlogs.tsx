f8551bfc2d49f4a641a874091d4697c8b0643f96
Muhammad3111 <karimovmuhammad3502@gmail.com>
2025-03-12 12:39:58 +0500

    update admin files

--- src/adminComponents/posts/ReadBlogs.tsx
+++ src/adminComponents/posts/ReadBlogs.tsx
@@ -1,75 +1,22 @@
 import { useState } from "react";
-import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-
-type Data = {
-  id: number;
-  image: string;
-  name: string;
-  brand: string;
-  applicable_models: string;
-  oem_code: string;
-  description: string;
-  status: boolean;
-  price: number;
-  vehicle_id: number;
-};
-
-const data: Data[] = [
-  {
-    id: 1,
-    image: "https://via.placeholder.com/50",
-    name: "Product 1",
-    brand: "Brand A",
-    applicable_models: "Model X, Model Y",
-    oem_code: "OEM12345",
-    description: "Description 1",
-    status: true,
-    price: 100,
-    vehicle_id: 1,
-  },
-  {
-    id: 2,
-    image: "https://via.placeholder.com/50",
-    name: "Product 2",
-    brand: "Brand B",
-    applicable_models: "Model Z",
-    oem_code: "OEM67890",
-    description: "Description 2",
-    status: false,
-    price: 200,
-    vehicle_id: 2,
-  },
-  // Qo'shimcha ma'lumotlar qo'shishingiz mumkin
-];
+import { useGetBlogsQuery } from "../../features/blogs/blogs";
 
 const ReadBlogs = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const [sortConfig, setSortConfig] = useState<{
-    key: keyof Data;
-    direction: "asc" | "desc";
-  } | null>(null);
 
-  const handleSort = (key: keyof Data) => {
-    setSortConfig((prevConfig) =>
-      prevConfig?.key === key && prevConfig.direction === "asc"
-        ? { key, direction: "desc" }
-        : { key, direction: "asc" }
-    );
-  };
+  const { data, isLoading } = useGetBlogsQuery({ page: 1 });
 
-  const filteredData = data.filter((item) =>
-    [item.name, item.brand, item.applicable_models].some((field) =>
+  if (isLoading) {
+    return <h1>Loading ...</h1>;
+  }
+
+  const blogs: Blogs[] = data?.items || [];
+
+  const filteredData = blogs.filter((item) =>
+    [item.title_uz].some((field) =>
       field.toLowerCase().includes(searchQuery.toLowerCase())
     )
   );
-
-  const sortedData = [...filteredData].sort((a, b) => {
-    if (!sortConfig) return 0;
-    const { key, direction } = sortConfig;
-    if (a[key] < b[key]) return direction === "asc" ? -1 : 1;
-    if (a[key] > b[key]) return direction === "asc" ? 1 : -1;
-    return 0;
-  });
 
   return (
     <div className="">
@@ -91,57 +38,32 @@ const ReadBlogs = () => {
             <tr>
               <th className="px-4 py-2">ID</th>
               <th className="px-4 py-2">Rasm</th>
-              <th className="px-4 py-2 flex items-center gap-1">
-                Nom{" "}
-                <button onClick={() => handleSort("name")}>
-                  {sortConfig?.key === "name" &&
-                  sortConfig.direction === "asc" ? (
-                    <IoIosArrowUp />
-                  ) : (
-                    <IoIosArrowDown />
-                  )}
-                </button>
-              </th>
-              <th className="px-4 py-2">Brand</th>
-              <th className="px-4 py-2">Avtomobil Modeli</th>
-              <th className="px-4 py-2">OEM Code</th>
-              <th className="px-4 py-2">Izoh</th>
-              <th className="px-4 py-2">Holat</th>
-              <th className="px-4 py-2 flex items-center gap-1">
-                Narx{" "}
-                <button onClick={() => handleSort("price")}>
-                  {sortConfig?.key === "price" &&
-                  sortConfig.direction === "asc" ? (
-                    <IoIosArrowUp />
-                  ) : (
-                    <IoIosArrowDown />
-                  )}
-                </button>
-              </th>
+              <th className="px-4 py-2 flex items-center gap-1">Sarlavha</th>
+              <th className="px-4 py-2">Bo'lim</th>
+              <th className="px-4 py-2">Content</th>
+              <th className="px-4 py-2 flex items-center gap-1">Xarakat </th>
             </tr>
           </thead>
           <tbody>
-            {sortedData.map((item) => (
+            {filteredData.map((item, index) => (
               <tr key={item.id} className="border-t">
-                <td className="px-4 py-2 text-center">{item.id}</td>
+                <td className="px-4 py-2 text-center">{index}</td>
                 <td className="px-4 py-2 text-center">
                   <img
-                    src={item.image}
-                    alt={item.name}
+                    src={item.cover_image || "blog-images.jpg"}
+                    alt={item.title_uz}
                     className="w-12 h-12 rounded-full mx-auto"
                   />
                 </td>
-                <td className="px-4 py-2">{item.name}</td>
-                <td className="px-4 py-2 text-center">{item.brand}</td>
-                <td className="px-4 py-2 text-center">
-                  {item.applicable_models}
+                <td className="px-4 py-2 line-clamp-1">{item.title_uz}</td>
+                <td className="px-4 py-2 text-center">{item.category}</td>
+                <td className="px-4 py-2 text-center line-clamp-1">
+                  {item.content_uz}
                 </td>
-                <td className="px-4 py-2 text-center">{item.oem_code}</td>
-                <td className="px-4 py-2 text-center">{item.description}</td>
                 <td className="px-4 py-2 text-center">
-                  {item.status ? "Active" : "Inactive"}
+                  <button>.</button>
+                  <button>.</button>
                 </td>
-                <td className="px-4 py-2 text-center">{item.price}</td>
               </tr>
             ))}
           </tbody>
